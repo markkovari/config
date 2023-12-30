3ce8d22f8ddd251bc6a62a4ab069a074cc6dc1dd
Mark Kovari <markkovari@Marks-MacBook-Pro.local>
2023-12-30 10:41:13 +0100

    chore: add gh cli

--- module/home-manager.nix
+++ module/home-manager.nix
@@ -2,6 +2,10 @@
 
 {
   # add home-manager user settings here
-  home.packages = with pkgs; [ git neovim ];
+  home.packages = with pkgs; [ 
+    git 
+    neovim
+    gh
+  ];
   home.stateVersion = "23.11";
 }
